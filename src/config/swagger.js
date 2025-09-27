fdf5a8d34ee053ee3a5dba1e5258af16f8b800c9
ehtasham <ehtashamtoor50@gmail.com>
2025-09-28 04:33:55 +0500

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -38,6 +38,7 @@ const setupSwagger = (app) => {
       customCssUrl: [
         "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
         "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
       ],
       customJs: [
         "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
